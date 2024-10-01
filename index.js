31567589662d2dff6db5ccd02c50f06bfc1a4478
cam96stanley <cam96stanley@gmail.com>
2024-09-30 22:35:40 -0400

    Added variables to js
    Created the variables to hold the DOM elements

---
+++ index.js
@@ -0,0 +1,5 @@
+const inputEl = document.getElementById("input-el");
+const convertBtn = document.getElementById("convert-btn");
+const lengthEl = document.getElementById("length-el");
+const volumeEl = document.getElementById("volume-el");
+const massEl = document.getElementById("mass-el");
